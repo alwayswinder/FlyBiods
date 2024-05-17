e0374b3a55e5d8fabf751d1837875246e7e808fa
alwasywinder <1181612350@qq.com>
2024-05-17 17:31:47 +0800

    ss

---
+++ ShaderTest/Public/MySimpleComputeShader.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GenericPlatform/GenericPlatformMisc.h"
+//#include "MySimpleComputeShader.generated.h"
+
+struct FMySimpleComputeShaderDispatchParams
+{
+	int X;
+	int Y;
+	int Z;
+
+	
+	int Input[2];
+	int Output = 0;
+	
+	
+
+	FMySimpleComputeShaderDispatchParams(int x, int y, int z)
+		: X(x)
+		, Y(y)
+		, Z(z)
+	{
+	}
+};
+
+// This is a public interface that we define so outside code can invoke our compute shader.
+class FMySimpleComputeShaderInterface {
+public:
+	// Executes this shader on the render thread
+	static void DispatchRenderThread(
+		FRHICommandListImmediate& RHICmdList,
+		FMySimpleComputeShaderDispatchParams Params,
+		TFunction<void(int OutputVal)> AsyncCallback
+	);
+
+	// Executes this shader on the render thread from the game thread via EnqueueRenderThreadCommand
+	static void DispatchGameThread(
+		FMySimpleComputeShaderDispatchParams Params,
+		TFunction<void(int OutputVal)> AsyncCallback
+	)
+	{
+		ENQUEUE_RENDER_COMMAND(SceneDrawCompletion)(
+		[Params, AsyncCallback](FRHICommandListImmediate& RHICmdList)
+		{
+			DispatchRenderThread(RHICmdList, Params, AsyncCallback);
+		});
+	}
+
+	// Dispatches this shader. Can be called from any thread
+	static void Dispatch(
+		FMySimpleComputeShaderDispatchParams Params,
+		TFunction<void(int OutputVal)> AsyncCallback
+	)
+	{
+		if (IsInRenderingThread()) {
+			DispatchRenderThread(GetImmediateCommandList_ForRenderCommand(), Params, AsyncCallback);
+		}else{
+			DispatchGameThread(Params, AsyncCallback);
+		}
+	}
+};
